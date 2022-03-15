d399ca46e09fc65c7c185ca96e7068731f8e0033
mohammad <mohammad.s@sequencebio.com>
2022-03-14 23:54:21 -0400

    Add comments

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -8,23 +8,28 @@ class Solution(object):
             return s
 
         ps = []
-            
+	
+	# add single palindromes
         for i in range(len(s)):
             ps.append([i,i])
             res = [i,i]
         
+	# add double palindromes
         for i in range(1, len(s)):
             if s[i-1] == s[i]:
                 ps.append([i-1, i])
                 res = [i-1, i]
+
+	# increase polindromes
         for p in ps:
             beg = p[0] - 1
             end = p[1] + 1
-            if beg>= 0 and  end< len(s):
+            if beg>= 0 and end< len(s):
                 if s[beg] == s[end]:
                     ps.append([beg, end])
                     res = [beg, end]
                     # print res
         
+	# last appended polindrome is the largest one
         return s[res[0]:res[1]+1]
 
